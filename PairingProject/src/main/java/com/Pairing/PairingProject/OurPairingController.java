cef8f91816f70c27c36a697863870a5ab7b076c1
Maria Xanthouli <maria2007x@gmail.com>
2022-11-07 15:54:26 +0200

    4rth commit with greetings

--- PairingProject/src/main/java/com/Pairing/PairingProject/OurPairingController.java
+++ PairingProject/src/main/java/com/Pairing/PairingProject/OurPairingController.java
@@ -8,18 +8,37 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class OurPairingController {
-    @PostMapping("/hello")
+//    @PostMapping("/hello")
+//    @ResponseBody
+//    public String hello(@RequestParam String name, String food){
+//        return "This is our pairing website. Welcome " + name + "! The recommended food of the day is " + food;
+//    }
+    @GetMapping("/")
     @ResponseBody
-    public String hello(@RequestParam String name, String food){
-        return "This is our pairing website. Welcome " + name + "! The recommended food of the day is " + food;
-    }
-@GetMapping("/")
-@ResponseBody
     public String post(){
-        return "<form action=\"/hello\" method=\"POST\">\n" +
+        return "<form action=\"/greetings\" method=\"POST\">\n" +
                 "<input name=\"name\" placeholder=\"Your name\">\n" +
-                "<input name=\"food\" placeholder=\"A meal\">\n" +
+                "<input name=\"language\" placeholder=\"language\">\n" +
                 "<button>Go</button>\n" +
                 "</form>";
     }
+
+    @PostMapping("/greetings")
+    @ResponseBody
+    public String greetings(@RequestParam String name, String language){
+        if (name.isEmpty()){
+            return "Please give me your name";
+        }
+
+        if (language.equals("en") || language.isEmpty()){
+            return "My name is " + name + ".";
+        } else if (language.equals("el")){
+            return "Με λένε " + name + ".";
+        } else if (language.equals("es")){
+            return "Me llamo " + name + ".";
+        } else {
+            return "Does not compute. Please try again";
+        }
+    }
+
 }
