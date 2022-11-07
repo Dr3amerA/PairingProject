ddbe40e7cae55c84dc279414fdef1e74d9b7d5e1
Alex <burnupalext_t@hotmail.com>
2022-11-07 12:59:41 +0200

    3rd commit post method

--- PairingProject/src/main/java/com/Pairing/PairingProject/OurPairingController.java
+++ PairingProject/src/main/java/com/Pairing/PairingProject/OurPairingController.java
@@ -2,14 +2,24 @@ package com.Pairing.PairingProject;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class OurPairingController {
-    @GetMapping("/hello")
+    @PostMapping("/hello")
     @ResponseBody
     public String hello(@RequestParam String name, String food){
         return "This is our pairing website. Welcome " + name + "! The recommended food of the day is " + food;
+    }
+@GetMapping("/")
+@ResponseBody
+    public String post(){
+        return "<form action=\"/hello\" method=\"POST\">\n" +
+                "<input name=\"name\" placeholder=\"Your name\">\n" +
+                "<input name=\"food\" placeholder=\"A meal\">\n" +
+                "<button>Go</button>\n" +
+                "</form>";
     }
 }
