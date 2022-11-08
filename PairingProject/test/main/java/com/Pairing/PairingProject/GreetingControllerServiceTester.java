fcd445c704ca8a63493a0187c8f8fe0ab92109eb
Maria Xanthouli <maria2007x@gmail.com>
2022-11-08 13:26:22 +0200

    some more testing

--- PairingProject/test/main/java/com/Pairing/PairingProject/GreetingControllerServiceTester.java
+++ PairingProject/test/main/java/com/Pairing/PairingProject/GreetingControllerServiceTester.java
@@ -1,22 +1,40 @@
 package main.java.com.Pairing.PairingProject;
 
+import com.Pairing.PairingProject.LangService;
 import com.Pairing.PairingProject.NameCountService;
 import org.junit.Test;
-import org.junit.jupiter.api.BeforeEach;
 
-import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class GreetingControllerServiceTester {
 
     private NameCountService nameCount = new NameCountService();
+    private LangService lang = new LangService();
 
 
 
     @Test
-  public  void ShouldBeRightLanguage_RightCount(){
+    public  void ShouldBeEqualsLanguage_RightCount(){
         assertEquals( "Times called for Maria: 1.",nameCount.countNames("Maria", "en"));
-        assertEquals( "Times called for Maria: 2.",nameCount.countNames("Maria", "en"));
+        assertEquals( "Το όνομα Maria εμφανίζεται 2 φορές.",nameCount.countNames("Maria", "el"));
+        assertEquals( "El nombre Maria se aparece 3 veces.",nameCount.countNames("Maria", "es"));
+
     }
+
+    @Test
+    public  void ShouldBeEqualLanguage(){
+        assertEquals( "My name is Maria!",lang.translate("Maria", "en"));
+        assertEquals( "Me llamo Maria!",lang.translate("Maria", "es"));
+        assertEquals( "Με λένε Maria!",lang.translate("Maria", "el"));
+    }
+
+    @Test
+    public  void FalseLanguage(){
+        assertFalse("My name is Maria!".equals(lang.translate("Maria", "es")));
+        assertFalse("Me llamo Maria!".equals(lang.translate("Maria", "el")));
+        assertFalse("Με λένε Maria!".equals(lang.translate("Maria", "es")));
+    }
+
+
 }
